cec32b204541b03c72311a812c6e28f9905e94d6
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-10 11:53:35 -0700

    feat: create Meals component, which makes an API call to then populate a loadedMeals state, this is wrapped in useEffect to prevent an infinite loop when the async function is called in the component and populates the list of meals in the component

---
+++ src/components/Meals.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export default function Meals() {
+  // CREATE STATE TO LOAD MEALS AFTER API CALL RUNS SUCCESSFULLY
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  // NEED TO WRAP WITH USEEFFECT TO RUN THIS API CALL ONLY ONCE WHEN THE APP RENDERS - PREVENTS INFINIATE LOOP
+  useEffect(() => {
+    // SET UP THE API CALL - USE ASYNC AND AWAIT TO MAKE API CALL AND WAIT FOR A RESPONSE(PROMISE)
+    async function fetchMeals() {
+      // GET IS THE DEFAULT METHOD AND WOULD BE USED IF NOT DEFINED, BUT INCLUDING AS AN EXAMPLE OF HOW TO DEFINE THE METHOD USED IN THE HTTP REQUEST
+      const response = await fetch("http://localhost:3000/meals", {
+        method: "GET",
+      });
+
+      // EXTRACT DATA FROM API REQUEST AND CONVERT TO JSON, NEED TO USE AWAIT BECAUSE RESPONSE IS EXPECTING A PROMISE
+      const meals = await response.json();
+      // UPDATE STATE TO POPULATE WITH MEALS ONCE API CALL RUNS SUCCESSFULLY
+      setLoadedMeals(meals);
+    }
+
+    // CALL FUNCTION TO RUN IT
+    fetchMeals();
+
+    //REMEMBER USE EFFECT DEPENDENCY ARRAY BEING EMPTY MEANS IT'LL JUST RUN ONCE ON THE FIRST RENDER, IF THERE WAS A DEPENDENCY IT WOULD RUN AGAIN IF THE DEPENDENCY CHANGES
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
