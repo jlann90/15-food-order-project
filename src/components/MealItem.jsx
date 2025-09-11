dcf93f02249219b7ff240671b06ad174bf06cfef
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-11 13:26:36 -0700

    feat: finish building out CartContext and Reducer, wrap App in our newly created context provider to make the context accessible, use this context in Header and MealItem

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+
 import Button from "./UI/Button.jsx";
 import { currencyFormatter } from "../util/formatting.js";
+import CartContext from "../store/CartContext.jsx";
 
 // WE PASS IN MEAL AS A PROP WHICH IS BRINGING IN ALL OF THE MEALS DATA, SINCE WE MAP THE LOADED MEAL IN THE MEALS COMPONENT
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -16,7 +24,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 // CREATE CONTEXT FOR SPREADING DATA TO OUR COMPONENTS - WILL NEED TO REFERENCE OUR ITEMS AND ALSO ADD AND REMOVE THEM
 const CartContext = createContext({
@@ -10,7 +10,7 @@ const CartContext = createContext({
 // CREATE OUR REDUCER FOR STATE MANAGEMENT
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
-    // CREATE A CONSTANT THAT CHECKS IF THE ITEM ALREADY EXISTS IN THE ITEMS ARRARY
+    // CREATE A CONSTANT THAT GETS THE CART ITEM INDEX (ITS PLACEMENT IN THE ARRAY) - ALLOWS TO EDIT IN IMMUTABLE WAY
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
@@ -20,7 +20,7 @@ function cartReducer(state, action) {
 
     // CHECKS IF THE ITEM HAS AN INDEX (BASICALLY IF IT EXISTS) - IF SO UPDATE THE QUANTITY
     if (existingCartItemIndex > -1) {
-      //CREATE A CONSTANT REPRESENTING AN EXISTING ITEM
+      //CREATE A CONSTANT REPRESENTING AN EXISTING ITEM'S VALUE (THE ITEM OBJECT AND ALL OF ITS PROPERTIES)
       const existingItem = state.items[existingCartItemIndex];
       //CREATE A NEW CONSTANT AND SPREAD THE EXISTING PROPERTIES INTO IT (ID, NAME, PRICE, ETC) BUT ALSO ADD A QUANTITY PROPERTY
       const updatedItem = {
@@ -35,11 +35,36 @@ function cartReducer(state, action) {
       updatedItems.push({ ...action.item, quantity: 1 });
     }
 
-    // RETURNS THE UPDATED STATE FOR THE "ADD_ITEM" TYPE
+    // RETURNS THE UPDATED STATE
     return { ...state, items: updatedItems };
   }
 
   if (action.type === "REMOVE_ITEM") {
+    // SAME APPROACH FOR GETTING THE CART ITEM INDEX AS ADDING AN ITEM ABOVE - ALLOWS TO EDIT IN IMMUTABLE WAY
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    // CREATE A CONSTANT FOR THE CURRENT ITEM
+    const existingCartItem = state.items[existingCartItemIndex];
+    // CREATE A COPY OF CURRENT STATE/ITEMS TO WORK WITH
+    const updatedItems = [...state.items];
+
+    // CHECKS IF THE ITEM ONLY HAS 1 IN THE CART AND REMOVE IT ENTIRELY IF SO
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+
+      // OTHERWISE IF THE ITEM HAS MORE THAN 1 IN THE CART, REDUCE THE QUANTITY BY 1
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    // RETURNS THE UPDATED STATE
+    return { ...state, items: updatedItems };
   }
 
   return state;
@@ -49,9 +74,29 @@ function cartReducer(state, action) {
 export function CartContextProvider({ children }) {
   // USING USEREDUCER RATHER THAN USESTATE BECAUSE ITS BETTER FOR MORE COMPLEX STATE MANAGEMENT, AND EASIER TO MOVE STATE MANAGEMENT OUT OF THE COMPONENT FUNCTION
   // WE CALL OUR REDUCER AS ONE ARGUMENT AND THE OBJECT SHAPE AS THE OTHER - SO WE'RE REFERENCIGN OUR CART CONTEXT SHAPE IN THAT SECOND ARGUMENT
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  // CREATE OUR FUNCTIONS WE DEFINED IN THE REDUCER FOR THE CART CONTEXT
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  // CREATE THE CONTEXT TO PASS INTO THE PROVIDER BELOW
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  console.log(cartContext);
+
   // ULTIMATELY WHAT WE ARE RETURNING IS A CART CONTEXT WRAPPER TO GO AROUND COMPONENTS THAT NEED THIS CONTEXT
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 // MAKE SURE TO EXPORT BOTH THE CONTEXT PROVIDER ABOVE AND THE CONTEXT
