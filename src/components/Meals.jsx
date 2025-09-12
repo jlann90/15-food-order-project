0fe16776400455ffa8d0799db4775c2fbcd4dac4
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-09-12 12:50:18 -0700

    feat: creat hooks folder and useHttp custom hook, update Meals to use useHttp for API call, the custom hook handles error and loading states as well

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,30 +1,47 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+
+const requestConfig = {};
 
 export default function Meals() {
-  // CREATE STATE TO LOAD MEALS AFTER API CALL RUNS SUCCESSFULLY
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  // NEED TO WRAP WITH USEEFFECT TO RUN THIS API CALL ONLY ONCE WHEN THE APP RENDERS - PREVENTS INFINIATE LOOP
-  useEffect(() => {
-    // SET UP THE API CALL - USE ASYNC AND AWAIT TO MAKE API CALL AND WAIT FOR A RESPONSE(PROMISE)
-    async function fetchMeals() {
-      // GET IS THE DEFAULT METHOD AND WOULD BE USED IF NOT DEFINED, BUT INCLUDING AS AN EXAMPLE OF HOW TO DEFINE THE METHOD USED IN THE HTTP REQUEST
-      const response = await fetch("http://localhost:3000/meals", {
-        method: "GET",
-      });
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
 
-      // EXTRACT DATA FROM API REQUEST AND CONVERT TO JSON, NEED TO USE AWAIT BECAUSE RESPONSE IS EXPECTING A PROMISE
-      const meals = await response.json();
-      // UPDATE STATE TO POPULATE WITH MEALS ONCE API CALL RUNS SUCCESSFULLY
-      setLoadedMeals(meals);
-    }
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error}></Error>;
+  }
+  // ORIGINAL WAY WE WERE DOING API CALL BEFORE CREATING CUSTOM HOOK
+  //   // CREATE STATE TO LOAD MEALS AFTER API CALL RUNS SUCCESSFULLY
+  //   const [loadedMeals, setLoadedMeals] = useState([]);
 
-    // CALL FUNCTION TO RUN IT
-    fetchMeals();
+  //   // NEED TO WRAP WITH USEEFFECT TO RUN THIS API CALL ONLY ONCE WHEN THE APP RENDERS - PREVENTS INFINIATE LOOP
+  //   useEffect(() => {
+  //     // SET UP THE API CALL - USE ASYNC AND AWAIT TO MAKE API CALL AND WAIT FOR A RESPONSE(PROMISE)
+  //     async function fetchMeals() {
+  //       // GET IS THE DEFAULT METHOD AND WOULD BE USED IF NOT DEFINED, BUT INCLUDING AS AN EXAMPLE OF HOW TO DEFINE THE METHOD USED IN THE HTTP REQUEST
+  //       const response = await fetch("http://localhost:3000/meals", {
+  //         method: "GET",
+  //       });
 
-    //REMEMBER USE EFFECT DEPENDENCY ARRAY BEING EMPTY MEANS IT'LL JUST RUN ONCE ON THE FIRST RENDER, IF THERE WAS A DEPENDENCY IT WOULD RUN AGAIN IF THE DEPENDENCY CHANGES
-  }, []);
+  //       // EXTRACT DATA FROM API REQUEST AND CONVERT TO JSON, NEED TO USE AWAIT BECAUSE RESPONSE IS EXPECTING A PROMISE
+  //       const meals = await response.json();
+  //       // UPDATE STATE TO POPULATE WITH MEALS ONCE API CALL RUNS SUCCESSFULLY
+  //       setLoadedMeals(meals);
+  //     }
+
+  //     // CALL FUNCTION TO RUN IT
+  //     fetchMeals();
+
+  //     //REMEMBER USE EFFECT DEPENDENCY ARRAY BEING EMPTY MEANS IT'LL JUST RUN ONCE ON THE FIRST RENDER, IF THERE WAS A DEPENDENCY IT WOULD RUN AGAIN IF THE DEPENDENCY CHANGES
+  //   }, []);
 
   return (
     <ul id="meals">
